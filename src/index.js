4c6e97af25e7b5b57e576e1d5ae1c708af8b2b1f
VasilevskayaT <unost.work@gmail.com>
2021-03-07 23:10:24 +0300

    feat: implement function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let joinedBrackets = bracketsConfig.map(item => item.join(''));
+
+    for (let brack of joinedBrackets) {
+        str = str.split(brack).join('')
+    }
+    let count = 0;
+    for (let brack of joinedBrackets) {
+        if (str.includes(brack)) count++;
+    }
+    if(count > 0) {
+        return check(str, bracketsConfig)
+    }
+
+return !Boolean(str)
+};
